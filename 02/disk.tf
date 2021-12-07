f6470dfe1de86604a9a10ec36875cc6bb7efca11
Matheus Passini <80512548+matheuspassini@users.noreply.github.com>
2021-12-07 15:19:31 -0300

    Delete 02 directory

--- 02/disk.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_disk" "disk" {
-  name = "disk"
-  type = "pd-ssd"
-  size = 30
-  zone = "us-central1-a"
-}
--- 02/network.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_network" "network" {
-  name = "vpc-network"
-  auto_create_subnetworks = true
-}
